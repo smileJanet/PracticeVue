d997af864e651c77f23a1769c2c3a1fd146c303f
Janet <juyeonge6@gmail.com>
2024-11-19 14:41:17 +0900

    2

---
+++ App.js
@@ -0,0 +1,8 @@
+export default { // 전체 컴포넌트를 묶은 App 컴포넌트를 외부(main)로 export함
+    template : `
+        <app-header></app-header>
+        <app-nav></app-nav>
+        <app-view></app-view>
+    `
+}
+
