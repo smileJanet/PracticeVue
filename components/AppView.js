7dac818323483229bc87e0341f52a75a01a85a49
Janet <juyeonge6@gmail.com>
2024-11-19 14:42:56 +0900

    component3

---
+++ components/AppView.js
@@ -0,0 +1,11 @@
+export default {  // AppView 컴포넌트를 외부로 export함
+    template : `
+        <main>
+        <section>
+            <book-component></book-component>
+            <book-component></book-component>
+            <book-component></book-component>
+        </section>
+        </main>
+    `
+} 
