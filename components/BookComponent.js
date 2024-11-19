615568df577ea27c14e2f2ef26d7b1f3d2c8cd4f
Janet <juyeonge6@gmail.com>
2024-11-19 14:43:16 +0900

    component4

---
+++ components/BookComponent.js
@@ -0,0 +1,9 @@
+export default {  // BookComponent 컴포넌트를 외부로 export함
+    template : 
+        `<article class="book">
+        <div class="book__subtitle">{{ subtitle }}</div>
+        <div class="book__title">
+            HTML 강좌
+        </div>
+        </article>`
+    }
