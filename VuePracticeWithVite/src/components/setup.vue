e92d806947aab76d54c47ad54d003af7f49221e5
Janet <juyeonge6@gmail.com>
2024-11-20 13:28:18 +0900

    Template Grammar

---
+++ VuePracticeWithVite/src/components/setup.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <p>
+      {{counter}}
+      </p>
+      <p>
+      {{message}}
+      </p>
+      <button @click="increment">click!</button>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue' // script태그 안에 import, export문을 작성한다.
+  
+  export default { 
+    setup() { // 
+      const counter = ref(0);
+      const message = ref('Hello World!');
+      const increment = () => {counter.value++};
+  
+      // this 키워드를 사용할수 없다. 값을 setup에 직접 노출시킬것.
+      return {
+        counter,
+        message,
+        increment, 
+      }
+    },
+    // Vue3은 Composition api를 주력으로 사용.
+    // vue3에서 사용하는 메소드 setup() 내 counter, message, increment는 this를 통해 접근할 수 없음.
+    // 왜냐면 this는 Options api에서 사용하는 방식이기 때문.
+    // 템플릿이나 다른 라이프사이클을 통해서 setup()값에 접근해야 한다.
+    //mounted() { // option api에서 사용함
+      //console.log(this.count)
+      //console.log(counter.value); // this키워드가 아닌 값.value를 통해 접근하도록
+    //}
+  }
+  </script>
+  
+  
+  
+  
---
+++ VuePracticeWithVite/src/components/template.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <hr>
+        <h2>보간법</h2>
+        <p v-once>{{ message }}</p>
+        <p>{{ message }}</p>
+        <button v-on:click="message = message + '!'">
+            Click!
+        </button>
+
+        <hr>
+        <h2>v-html</h2>
+        <!-- v-html태그는 {{  }}이렇게 쓰는 것이 아니다. 태그 안에 작성할 것 -->
+        <!-- v-html은 악성 스크립트에 취약한 디렉티브이기 때문에 사용 시 주의해야 한다  -->
+        <span v-html="rawHtml"></span> 
+
+        <hr>
+        <h2>v-bind(속성 바인딩)</h2>
+        <div v-bind:title="dynamicTitle"> 마우스를 올려보시오 </div>
+
+        <!-- v-bind는 자주 사용하기 때문에 단축키워드가 있다 (:) -->
+        <div :title="dynamicTitle"> 마우스를 올려보시오 </div>
+        <input type="text" value="홍길동" v-bind:disabled="isDisabled" /> &nbsp;
+
+        <!-- 다중 속성 바인딩 -->
+        <!-- 여러개의 속성을 한번에 바인딩할 수 있다. -->
+        <input v-bind="attrs" />
+
+        <hr>
+        <h2>JavaScript 표현식 사용</h2>
+        <!-- {{}} 안에선 JavaScript 표현식을 사용할 수 있다. -->
+        {{ message.split('').reverse().join('') }} &nbsp;
+        {{ isDisabled ? 'Yes' : 'No' }}
+
+    </div>
+</template>
+
+<script>
+    import {ref} from 'vue';
+
+    export default{
+        setup(){
+            const message = ref ('Hello, Janet!');
+            const rawHtml = ref('<h4 style="color:royalblue;">안녕하세요</h4>');
+            const dynamicTitle = ref('Hello, PrettyGirl!');
+            const isDisabled = ref(false);
+            const attrs = ref({ // 객체이므로 {}에 감싸야한다. 잊지 말것~
+                type:'text',
+                value:'123456',
+                disabled: false,
+            });
+
+            return{
+                message,
+                rawHtml,
+                dynamicTitle,
+                isDisabled,
+                attrs,
+            };
+        },
+    };
+</script>
+
+<style lang="scss" scoped></style>
