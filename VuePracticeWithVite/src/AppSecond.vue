6a0bd1fafccbcf95ffed1423ef6c064623e89354
Janet <juyeonge6@gmail.com>
2024-11-24 16:35:36 +0900

    before fragment

--- VuePracticeWithVite/src/AppSecond.vue
+++ VuePracticeWithVite/src/AppSecond.vue
@@ -1,36 +1,65 @@
 <template>
-
+<div>
 <div id="app" style="border: 1px solid #3CB371; border-radius:5%;">
     <h2 style="color:	#3CB371;">Hello, Vue3!</h2>
     
     <div class="row g-1">
 
     <div class="col col-4">
-    <Props title="하드코딩 제목" contents="하드코딩 내용"/> <!--하드코딩 방식-->
+    <Props title="하드코딩 제목" contents="하드코딩 내용"/> 
+    <!--하드코딩 방식-->
     </div>
 
     <div class="col col-4">
-    <Props :title="post.title" :contents="post.contents" /> <!--객체에 접근하는 방식(v-bind이용해서)-->
+    <Props :title="post.title" :contents="post.contents" /> 
+    <!--객체에 접근하는 방식(v-bind이용해서)-->
     </div>
 
     <div v-for="post in posts" :key="post.id" class="col col-4">
     <!-- Vue에선 컴포넌트에서 선언할 땐 PascalCase, templete안에서 쓸 땐 kebab-case로 작성한다(앞 강의 참조) -->
     <!-- Props컴포넌트에선 isLike로 선언했지만, 부모 컴포넌트(AppSecond)에서 사용될 땐 is-like로 작성해도 문제 없다 -->
-    <Props 
-    :title="post.title" 
-    :contents="post.contents" 
-    :type="post.type" 
-    :is-like="post.isLike" 
-    @toggle-like="post.isLike = !post.isLike" />
+    
+    <!-- <Props :title='post.title' :contents='post.contents' :type='post.type' :is-like='post.isLike' @toggle-like="post.isLike = !post.isLike" /> -->
 
     </div>
 
     </div>
+    
+    <hr />
+    
+    <!-- label태그에 상속이 되있는 것을 확인할 수 있다 -->
+    <!-- 부모컴포넌트 내에서 자식 컴포넌트의 class, id, style 등을 부여하면  -->
+    <!-- 자식 컴포넌트 내 루트 요소인 label태그에서 해당 속성들을 상속받은 것을 확인할 수 있다  -->
+    
+    <!-- 만약 부모 컴포넌트와 자식 컴포넌트 모두 class, style, id 등이 정의되어 있다면 ? -->
+    <!-- class와 style은 부모 속성 + 자식 속성으로 병합되고 -->
+    <!-- 나머지 속성(ex. id)은 부모 속성으로 덮어쓰기 된다  -->
+    
+    <!-- 
+    <NonProps
+    v-model="username"
+        label='name'
+        class='parent-class'
+        style='color:red'
+        id='parent-id'
+        />
+    -->    
+    
+    <!--  v-on 이벤트 리스너 실습 -->
+    <!-- non-props 속성은 자식 컴포넌트의 루트 속성에 상속된다.  -->
+    <NonProps 
+        @click="sayHello"
+        class="my-button"
+        id="my-button"
+    />
+    <!-- 그런데 class와 id는 상속하고 싶을 때? -->
+    <!-- 내장 객체 attribute를 사용한다 -->
 </div>
-
+</div>
 </template>
 
-<script> // options api방식
+<script> 
+// options api방식
 // advanced - components 
 // 에러가 너무 많아서 셋팅이 오래 걸렸다.
 // props라는 예약어가 있으므로, props가 아닌 Props로 써야 올바르게 컴포넌트가 컴파일 될 수 있다.
@@ -39,13 +68,16 @@
 // script setup  composition api방식, setup(){}이 겸용된 상태. import만 하면 컴포넌트 사용 가능
 // 지금 파일과 같은 script 내 components :{} 방식 : options api 방식, import + components{}객체에 명시해야 함. 좀 더 복잡하고 어려움. 
 
+
 import Props from './advancedComponents/props.vue'
+import NonProps from './advancedComponents/NonProps.vue'
 import {reactive} from  'vue'
 
 export default {
 
     components:{ 
         Props,
+        NonProps,
     },
 
     setup(){
@@ -61,9 +93,15 @@ export default {
         {id:4, title : 'title4', contents : 'contents4', type:'news', isLike : false,},
     ]);
 
+    const sayHello = () => {
+        alert('안녕하세요!');
+    }
+
     return {
         post,
         posts,
+        sayHello,
+
         };
     }
 }
---
+++ VuePracticeWithVite/src/advancedComponents/NonProps.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- <label     
+    class='child-class'
+    style='border:1px solid red; padding: 10px; border-radius:10px;'
+    id='child-id'>
+    {{ label }}
+    <input v-model="value" type="text">
+  </label> -->
+    <div class="p-3 bg-warning">
+    <button class="btn btn-primary" type="button" v-bind="$attrs"> Button!  </button>
+    <!-- {{ $attrs }}를 사용해서 부모 컴포넌트에서 정해진 class, id를 상속받을 수 있다 -->
+    <!-- {{ $attrs }} 이것은 setup의 두번째 객체 context에서 사용할 수 있다.  -->
+    <!-- v-bind=$attrs를 통해서 부모 컴포넌트에 적용된 non-props들을 자식 컴포넌트의 루트 속성이 아닌 다른 속성에도 적용할 수 있다. -->
+    </div>
+
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+    props :['modelValue', 'label'],
+    emits : ['update:modelValue'],
+
+    // 상속 거부는 자식 컴포넌트에서 해야 한다.
+    inheritAttrs : false,
+
+    setup(props, context){
+      context.attrs
+      console.log(context.attrs);
+      console.log(context.attrs.class);
+      console.log(context.attrs.id);
+      console.log(context.attrs.onClick);
+      const value = computed({
+        get(){
+          return props.modelValue;
+        },
+        set(value){
+          emit('update:modelValue', value);
+        },
+      });
+
+      return {
+        value
+      };
+    },
+    
+};
+</script>
+
+<style>
+
+</style>
